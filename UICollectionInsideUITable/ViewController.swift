e744299d1dbd6d6481d873759bff8c1c8b07f1ff
gulsah-altiparmak <42993752+gulsah-altiparmak@users.noreply.github.com>
2021-02-19 18:01:57 +0300

    Initial Commit

---
+++ UICollectionInsideUITable/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UICollectionInsideUITable
+//
+//  Created by Gulsah Altiparmak on 19.02.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
