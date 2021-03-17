41beaa9e65234cff217f1b39f42132fe742c2bde
gulsah-altiparmak <42993752+gulsah-altiparmak@users.noreply.github.com>
2021-03-17 14:15:30 +0300

    Update Constants.swift

--- UICollectionInsideUITable/Operations/Constants.swift
+++ UICollectionInsideUITable/Operations/Constants.swift
@@ -8,7 +8,7 @@
 import Foundation
 class Constants{
     static let baseUrl = "https://api.themoviedb.org/3/"
-   static  let apiKey = "dfa1447aa22cc51e7d90fd37215329fe"
+   static  let apiKey = "yourApiKey"
     static let sortValue = ["popularity.desc","release_date.desc","revenue.desc", "primary_release_date.desc","original_title.desc","vote_average.desc","vote_count.desc"]
     static let sortTitle = ["Popular","Release Date","Revenue","Primary Release"]
     static  let imageBaseUrl = "https://image.tmdb.org/t/p/w300"
